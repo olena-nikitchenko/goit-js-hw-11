589793c19e5648c08484f38ba42f17e4e9760aef
olena-nikitchenko <helnikitka@gmail.com>
2023-03-19 21:19:16 +0200

    add solution search for name

---
+++ src/index.js
@@ -0,0 +1,133 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { fetchImages } from './js/fetchImages';
+
+const searchForm = document.querySelector('#search-form');
+const inputEl = document.querySelector('input[name="searchQuery"]');
+const divGallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+
+let perPage = 40;
+let page = 0;
+let name = inputEl.value;
+btnHidden();
+
+async function onSearch(e) {
+    e.preventDefault();
+    clear();
+    name = inputEl.value.trim();
+    page = 1;
+    if (name === '') {
+        Notiflix.Notify.failure(`Enter a name to search!`);
+        return;
+    }
+    fetchImages(name, page, perPage)
+        .then(name => {
+            console.log(name.hits);
+            let totalPages = name.totalHits / perPage;
+            if (name.hits.length > 0) {
+                Notiflix.Notify.success(`Hooray! We found ${name.totalHits} images.`);
+                renderImage(name);
+                lightbox();
+                if (page < totalPages) {
+                    btnIsNotHidden();
+                } else {
+                    Notiflix.Notify.info(
+                        "We're sorry, but you've reached the end of search results."
+                    );
+                }
+            } else {
+                Notiflix.Notify.failure(
+                    'Sorry, there are no images matching your search query. Please try again.'
+                );
+                clear();
+            }
+        })
+        .catch(error => console.log(error.message));
+}
+
+function renderImage(name) {
+    const markup = name.hits
+        .map(hit => {
+            return `<div class="photo-card">
+              <a class="gallery-item" href="${hit.largeImageURL}">
+              <img
+                 class="gallery-image"
+                src="${hit.webformatURL}"
+                  alt="${hit.tags}"
+                   loading="lazy"
+              /></a>
+
+               <div class="info">
+                 <div class="info-box">
+                   <p class="info-item">
+                   <b>Likes</b>
+                  </p>
+                  <p class="info-value">${hit.likes.toString()}</p>
+                </div>
+
+                 <div class="info-box">
+                   <p class="info-item">
+                     <b>Views</b>
+                   </p>
+                   <p class="info-value">${hit.views.toString()}</p>
+                 </div>
+
+                 <div class="info-box">
+                   <p class="info-item">
+                     <b>Comments</b>
+                   </p>
+                   <p class="info-value">${hit.comments.toString()}</p>
+                 </div>
+
+                 <div class="info-box">
+                   <p class="info-item">
+                     <b>Downloads</b>
+                   </p>
+                   <p class="info-value">${hit.downloads.toString()}</p>
+                 </div>
+
+               </div>
+             </div>`;
+        })
+        .join('');
+    divGallery.insertAdjacentHTML('beforeend', markup);
+}
+
+searchForm.addEventListener('submit', onSearch);
+
+function clear() {
+    divGallery.innerHTML = '';
+}
+function btnHidden() {
+    loadMoreBtn.classList.toggle('is-hidden');
+}
+function btnIsNotHidden() {
+    loadMoreBtn.classList.toggle('is-hidden');
+}
+window.addEventListener(
+    'scroll',
+    () => {
+        name = inputEl.value;
+        page += 1;
+        fetchImages(name, page, perPage).then(name => {
+            let totalPages = name.totalHits / perPage;
+            renderImage(name);
+            lightbox();
+
+            if (page >= totalPages) {
+                Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+            }
+        });
+    },
+    true
+);
+
+function lightbox() {
+    new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+        animationSpeed: 500,
+    });
+}
